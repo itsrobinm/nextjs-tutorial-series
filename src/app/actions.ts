00d980a9ed91c77d91e88197992895cec2073e95
Robin Moffatt <robinmoffatt5@gmail.com>
2025-04-24 13:24:50 +0100

    introduction to SSR, client components & server actions

---
+++ src/app/actions.ts
@@ -0,0 +1,9 @@
+"use server";
+
+import { products } from "@/globals/globals";
+import { revalidatePath } from "next/cache";
+
+export const addProduct = async (product: string) => {
+    products.push({ name: product });
+    revalidatePath("/");
+};
